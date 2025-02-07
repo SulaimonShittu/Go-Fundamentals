987855c41d9c06adbd63d69361b800abbbf79e62
SulaimonShittu <71654123+SulaimonShittu@users.noreply.github.com>
2025-02-07 17:26:24 +0100

    Course end

--- main.go
+++ main.go
@@ -1,11 +1,60 @@
-package Go_Fundamentals
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	var name string = "Dent, Arthur"
-	var score = 87
 
-	fmt.Println("Student Scores")
-	fmt.Println(name, score)
+	scores := []score{}
+	shouldContinue := true
+
+	for shouldContinue {
+		fmt.Println("1) Enter a score")
+		fmt.Println("2) Print report")
+		fmt.Println("q) Quit")
+		fmt.Println()
+		fmt.Println("Please select an option")
+
+		var option string
+		fmt.Scanln(&option)
+
+		switch option {
+		case "1":
+			scores = append(scores, addStudent())
+		case "2":
+			printReport(scores)
+		case "q":
+			shouldContinue = false
+		}
+	}
+}
+
+type score struct {
+	name  string
+	score int
+}
+
+func printReport(scores []score) {
+	fmt.Println()
+	fmt.Println("Student scores")
+	fmt.Println(strings.Repeat("-", 14))
+	for _, v := range scores {
+		fmt.Println(v.name, v.score)
+	}
+	fmt.Println(strings.Repeat("-", 14))
+	fmt.Println()
+}
+
+func addStudent() score {
+	fmt.Println("Enter a student name and score")
+	var name, rawScore string
+	fmt.Scanln(&name, &rawScore)
+	s, _ := strconv.Atoi(rawScore)
+	return score{
+		name:  name,
+		score: s,
+	}
 }
